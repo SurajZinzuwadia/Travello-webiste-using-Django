d51f6f5137e8cfbd847c533d7a8cfd2336583cad
Suraj Zinzuwadia <suraj.zinzuwadia@gmail.com>
2019-12-26 16:57:59 +0530

    Completing registration and login

--- telusko/accounts/urls.py
+++ telusko/accounts/urls.py
@@ -2,4 +2,12 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("register",views.register,name='register'),
+    path("login",views.login,name="login"),
+    path("logout",views.logout,name="logout"),
+    path("mumbai",views.destination.mumbai,name="mumbai"),
+    path("hydrabad",views.destination.hydrabad,name="hydrabad"),
+    path("bengaluru",views.destination.bengaluru,name="bengaluru"),
+    path("pune",views.destination.pune,name="pune"),
+    path("surat",views.destination.surat,name="surat"),
+    
 ]
--- telusko/accounts/views.py
+++ telusko/accounts/views.py
@@ -21,9 +21,43 @@ def register(request):
                 user = User.objects.create_user(username=username,password=password1,email=email,first_name=first_name,last_name=last_name)
                 user.save()
                 print('user created')
+                return redirect('login')
         else:
             messages.info(request,'password not matching')
             return redirect('register')
         return redirect('/')
     else:
         return render(request,'register.html')
+
+def login(request):
+    if request.method =='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect("/")
+        else:
+            messages.info(request,"Invalid credentials")
+            return redirect('login')
+        
+    else:
+        return render(request,'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+class  destination:
+    def mumbai(request):
+        return render(request,'mumbai.html')
+    def hydrabad(request):
+        return render(request,'hydrabad.html')
+    def pune(request):
+        return render(request,'pune.html')
+    def surat(request):
+        return render(request,'surat.html')
+    def bengaluru(request):
+        return render(request,'bengaluru.html')
+
